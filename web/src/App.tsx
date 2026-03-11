c2daf4f8fdbc2b8263bdb59c5614d43ab54ecacd
Juliet <m@juli.ee>
2026-03-11 16:04:47 +0100

    use dvh

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,10 @@ import type { ParentProps } from "solid-js";
 
 export default function App(props: ParentProps) {
   return (
-    <div class="text-text flex min-h-screen flex-col items-center justify-center px-4 font-sans">
+    <div class="text-text flex min-h-dvh flex-col items-center justify-center px-4 font-sans">
       <div class="mt-6 w-full max-w-2xl">{props.children}</div>
       <a
-        class="text-muted hover:text-accent-hover mt-auto px-4 pt-4 text-[10px]"
-        style={{ "padding-bottom": "max(1rem, env(safe-area-inset-bottom))" }}
+        class="text-muted hover:text-accent-hover mt-auto p-4 text-[10px]"
         href="https://github.com/notjuliet/drop"
       >
         source code
